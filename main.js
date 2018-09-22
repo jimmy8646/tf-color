25df19ee6d6da4718921de9e5a51614a8ac0d444
jimmy8646 <yujiechueh@gmail.com>
2018-09-22 13:40:26 +0800

    標準化數據

--- main.js
+++ main.js
@@ -1,7 +1,21 @@
+let data
+let colors = []
+let xs
+
+function preload() {
+  data = loadJSON('color.json')
+}
+
 function setup() {
-  
+
+  for (let record of data.entries) {
+    let col = [record.r / 255, record.g / 255, record.b / 255] //標準化
+    colors.push(col)
+  }
+  xs = tf.tensor2d(colors)
+  // console.log(xs.shape) [row,col]
 }
 
 function draw() {
-  
+
 }
