f54f85d7c64fa3b1b6e9dd77e0c469057b09ecba
jimmy8646 <yujiechueh@gmail.com>
2018-09-22 16:13:17 +0800

    xs,ys data

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 let data
 let colors = []
-let xs
+let xs,ys
+let labels = []
+let labelList = [
+  'red-ish',
+  'green-ish',
+  'blue-ish',
+  'orange-ish',
+  'yellow-ish',
+  'pink-ish',
+  'purple-ish',
+  'brown-ish',
+  'grey-ish',
+]
+
 
 function preload() {
   data = loadJSON('color.json')
@@ -9,11 +22,18 @@ function preload() {
 function setup() {
 
   for (let record of data.entries) {
-    let col = [record.r / 255, record.g / 255, record.b / 255] //標準化
+    let col = [record.r / 255, record.g / 255, record.b / 255] //Normalization
     colors.push(col)
+    labels.push(labelList.indexOf(record.label))
   }
   xs = tf.tensor2d(colors)
-  // console.log(xs.shape) [row,col]
+  // console.log(xs.shape) //[row,col]
+
+  let labelsTensor = tf.tensor1d(labels,'int32') 
+  // labelsTensor.print()
+
+  ys = tf.oneHot(labelsTensor,9)
+  
 }
 
 function draw() {
