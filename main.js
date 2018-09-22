20f943e85c1c58f475a184811164f746749f9766
jimmy8646 <yujiechueh@gmail.com>
2018-09-22 18:57:43 +0800

    train finish

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ let colors = []
 let xs, ys
 let model
 let labels = []
+let rSlider, gSlider, bSlider
 let labelList = [
   'red-ish',
   'green-ish',
@@ -21,7 +22,9 @@ function preload() {
 }
 
 function setup() {
-
+  rSlider = createSlider(0, 255, 255);
+  gSlider = createSlider(0, 255, 0);
+  bSlider = createSlider(0, 255, 255);
   for (let record of data.entries) {
     let col = [record.r / 255, record.g / 255, record.b / 255] //Normalization
     colors.push(col)
@@ -58,15 +61,53 @@ function setup() {
     loss: 'categoricalCrossentropy'
   })
 
-  const options = {
-    epochs: 10,
-  }
-
-  model.fit(xs, ys, options).then((res) => {
+  train().then((res) => {
     console.log(res.history.loss)
   })
 }
 
-function draw() {
+async function train() {
 
+  const options = {
+    epochs: 10,
+    validationSplit: 0.1, // 10%
+    shuffle: true,
+    callbacks: {
+      onTrainBegin: () => console.log('onTrainBegin'),
+      onTrainEnd: () => console.log('onTrainEnd'),
+      onEpochBegin: () => console.log('onEpochBegin'),
+      onEpochEnd: (num, logs) => {
+        console.log(`epochs: ${num} 結束， Loss值: ${logs.val_loss}`)
+        // console.log(logs)
+      },
+      onBatchBegin: () => console.log('onBatchBegin'),
+      onBatchEnd: () => {
+        // console.log('onBatchEnd')
+        return tf.nextFrame()
+      }
+    }
+  }
+
+  return await model.fit(xs, ys, options)
+}
+
+function draw() {
+  let r = rSlider.value();
+  let g = gSlider.value();
+  let b = bSlider.value();
+  background(r, g, b);
+  strokeWeight(2);
+  stroke(255);
+  line(frameCount % width, 0, frameCount % width, height);
+
+  const xs = tf.tensor2d([
+    [r / 255, g / 255, b / 255]
+  ])
+
+  let result = model.predict(xs)
+  let index = result.argMax(1).dataSync() //拿取機率最高的index
+
+  let label = labelList[index]
+  console.log(label)
+  // result.print()
 }
